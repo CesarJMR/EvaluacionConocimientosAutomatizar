41a94bc4e13e28f38afcf0c0438a373b8b5b6cfe
César Mena <cesarjosemena@outlook.com>
2020-01-15 20:48:50 -0400

    Estructuración2

---
+++ src/CasoPrueba1.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class CasoPrueba1 {
+
+    static WebDriver driver;
+    static String chromeDriver = System.getProperty("user.dir") + "\\drivers\\chromedriver.exe";
+
+    public static void main(String [] args){
+        System.setProperty("webdriver.chrome.driver", chromeDriver);
+        String baseURL = "https://www.amazon.com/";
+
+        try{
+            //Abro el navegador Y entro a la URL
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get(baseURL);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            WebDriverWait waitVar = new WebDriverWait(driver, 10);
+
+        }
+
+        catch (WebDriverException we){
+            System.err.println("\nEl WebDriver falló: " + we.getMessage());
+            we.printStackTrace();
+        }
+
+        catch (Exception ex){
+            System.err.println("\nHubo un fallo en el programa: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+
+
+
+
+
+
+    }
+}
